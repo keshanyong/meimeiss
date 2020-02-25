541d5858a86ddec56ec1734585ef8cc08bfabd76
modify <chenshuohn@126.com>
2020-02-25 21:03:53 +0800

    添加自定义打包

--- src/main/java/com/example/meimeiss/MeimeissApplication.java
+++ src/main/java/com/example/meimeiss/MeimeissApplication.java
@@ -2,9 +2,16 @@ package com.example.meimeiss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MeimeissApplication {
+public class MeimeissApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MeimeissApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MeimeissApplication.class, args);
